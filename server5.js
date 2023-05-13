24b06421e98065456c11fb979b4c7d4ea6578141
tejas <tejasshah@gmail.com>
2023-05-13 18:49:38 +0530

    array basic and session controller

---
+++ server5.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const sessionController = require("./controller/sessionController")
+const app = express()
+
+//middle 
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+
+//controller 
+app.post("/signup",sessionController.signup)
+app.get("/getallusers",sessionController.getAllUsers)
+
+app.listen(9999)
